33260a9bcd627608d9c7571cdcb8530f6aa8c317
永田健作 <nagatarock2@gmail.com>
2021-03-13 21:47:12 +0900

    スライダーをプラグインに変更。 投稿記事をスライダーにするのではなくて、自分で画像を設定出来るようにした。 「meta slider」

--- home.php
+++ home.php
@@ -1,14 +1,5 @@
 <?php get_header() ?>
-<?php global $paged;
-if ((empty($paged) || $paged == 1) && siteorigin_setting('slider_enabled')) : get_template_part('slider', 'home'); ?>
-<?php else : ?>
-	<div id="page-title" class="archive-title">
-		<div class="container">
-			<h1><?php echo esc_html(siteorigin_setting('general_latest_posts')) ?></h1>
-		</div>
-	</div>
-	<div id="home-slider-below"></div>
-<?php endif; ?>
+<?php echo do_shortcode('[metaslider id="179"]'); ?>
 <section class="p-home__service">
 	<div class="c-home__title">
 		<h2>
